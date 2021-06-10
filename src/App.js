c6174bc007647e0622228bc2c8b83c15746bc8a8
Morabito Federico <morabitofj@gmail.com>
2021-06-09 23:50:11 -0300

    Sign In Screen and funtionalities added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SignInScreen from './screens/SignInScreen';
 
 function App() {
 
     const cart = useSelector(state => state.cart);
-    const {cartItems} = cart;
+    const { cartItems } = cart;
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo } = userSignin;
+    const dispatch = useDispatch();
+
+    const signoutHandler = () => {
+        dispatch(signout());
+    }
 
     return (
         <BrowserRouter>
@@ -20,12 +29,20 @@ function App() {
                     <div>
                         <Link to="/cart">Cart</Link>
                         {cartItems.length > 0 && (<span className="badge">{cartItems.length}</span>)}
-                        <Link to="/signin">Sign In</Link>
+                        {userInfo ? (
+                            <div className="dropdown">
+                                <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i></Link>
+                                <ul className="dropdown-content">
+                                    <Link to="signout" onClick={signoutHandler}>Sign Out</Link>
+                                </ul>
+                            </div>)
+                            : <Link to="/signin">Sign In</Link>}
                     </div>
                 </header>
                 <main>
                     <Route path="/cart/:id?" component={CartScreen}></Route>
                     <Route path="/product/:id" component={ProductScreen}></Route>
+                    <Route path="/signin" component={SignInScreen}></Route>
                     <Route path="/" component={HomeScreen} exact></Route>
                 </main>
                 <footer className="row center">All right reserved</footer>
